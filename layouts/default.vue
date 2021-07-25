4cff5c7413f30e4a2feb9add3cdb9b3398d3b7b7
Abl <fotedev15@gmail.com>
2021-07-25 08:53:01 +0600

    'forgott'

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container  max-w-screen-lg">
+    <Header/>
+    <div class="bg-white py-2 px-4">
+  <Nuxt />
+  </div>
+  </div>
+</template>
+
+<script>
+import Header from '~/components/Header.vue'
+export default {
+  components: { Header },
+  head: {
+    bodyAttrs: {
+      class: 'bg-blue-200'
+    }
+  }
+}
+</script>
